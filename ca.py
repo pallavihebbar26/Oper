b753f89fadb8b213145363d19911e229cac083b5
Manjari <01fe24ica060kletech.ac.in>
2025-11-26 17:32:32 +0530

    friend commit

--- ca.py
+++ ca.py
@@ -4,8 +4,18 @@ def addition(a, b):
 def subtraction(a, b):
     return a - b
 
-if __name__ == "__main__":
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Error! Division by zero."
+    return a / b
+
+if __name__== "__main__":
     x = 20
     y = 10
     print("Addition:", addition(x, y))
     print("Subtraction:", subtraction(x, y))
+    print("Multiplication:", multiplication(x, y))
+    print("Division:", division(x, y))
