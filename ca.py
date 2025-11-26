7b7ad3f21caad4aea1dff32baf81ed851c8a24fe
Pallavi.Hebbar <01fe24ica003@kletech.ac.in>
2025-11-26 17:17:42 +0530

    first commit

---
+++ ca.py
@@ -0,0 +1,11 @@
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    x = 20
+    y = 10
+    print("Addition:", addition(x, y))
+    print("Subtraction:", subtraction(x, y))
